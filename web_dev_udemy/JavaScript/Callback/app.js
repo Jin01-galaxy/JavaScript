037dc1d15403fa46672fd208d99f999d3ef000ae
Jin01-galaxy <swiss10glacier@gmail.com>
2023-09-02 18:08:03 +0900

    commit10

--- web_dev_udemy/JavaScript/Callback/app.js
+++ web_dev_udemy/JavaScript/Callback/app.js
@@ -6,42 +6,57 @@ const numbers = [1,2,3,4,5,6];
 
 
 //215
-for (const iterator of numbers) {
-    console.log(iterator);
-}
+// for (const iterator of numbers) {
+//     console.log(iterator);
+// }
 
-//216 map
-const doubles = numbers.map(function(num) {
-    return num * 2;
-});
+// //216 map
+// const doubles = numbers.map(function(num) {
+//     return num * 2;
+// });
 
 
-const movies = [
-    {
-        title: 'Amadeus',
-        score: '99'
-    },
-    {
-        title: 'PPP',
-        score: '74'
-    },
-    {
-        title: 'Rockman',
-        score: '73'
-    },
-];
+// const movies = [
+//     {
+//         title: 'Amadeus',
+//         score: '99'
+//     },
+//     {
+//         title: 'PPP',
+//         score: '74'
+//     },
+//     {
+//         title: 'Rockman',
+//         score: '73'
+//     },
+// ];
 
-const titles = movies.map(function(movies) {
-    return movies.title;
-});
+// const titles = movies.map(function(movies) {
+//     return movies.title;
+// });
 
 
 //217 arrow_func
-const add = (x, y) => {
-    return x + y;
+// const add = (x, y) => {
+//     return x + y;
+// }
+
+// const rollDie = () => {
+//     return Math.floor(Math.random() * 6) + 1;
+// } 
+
+
+const user = {
+    firstName: "Harvey",
+    lastName: "Milk"
 }
 
-const rollDie = () => {
-    return Math.floor(Math.random() * 6) + 1;
-} 
 
+//233
+// const fullName = ({first, last}) => {
+//     return `${first} ${last}`
+// }
+
+function fullName({ firstName, lastName}) {
+    return `${firstName} ${lastName}`;
+}
